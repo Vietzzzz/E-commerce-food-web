cc9ebe11606e1f5202d74e0427554a1af76346e4
Vietzzzz <doanhviet7777@gmail.com>
2025-03-30 18:36:10 +0700

    update database

--- userauths/models.py
+++ userauths/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100)
-    bio = models.CharField(max_length=50)
+    bio = models.CharField(max_length=50, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
