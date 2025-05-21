363b40768c3e2a03f53618941b9ea890600ee0be
Gnaht <rexming2016@gmail.com>
2025-05-21 14:49:03 +0700

    add chatbot mini window

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = "chatbot"
+
 urlpatterns = [
-    path("", views.chatbot_view, name="chatbot_view"),
+    path("", views.chatbot_view, name="chatbot_interface"),
     path("get-response/", views.get_chatbot_response, name="get_chatbot_response"),
 ]
