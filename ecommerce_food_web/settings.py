aaf1c928cba35dd6af04c0b5217beb9c79c778fe
Gnaht <rexming2016@gmail.com>
2025-07-09 13:03:31 +0700

    Update settings.py

--- ecommerce_food_web/settings.py
+++ ecommerce_food_web/settings.py
@@ -38,7 +38,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
